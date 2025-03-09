a4fa594548477f81f3161f6a8dbba9838fa0593a
bek-shoyatbek <shoyatbekov03032003@gmail.com>
2025-03-10 01:45:29 +0500

    fix: mailer service fixed according to new templates module

--- src/modules/mail/mail.service.ts
+++ src/modules/mail/mail.service.ts
@@ -1,13 +1,15 @@
 import { MailerService } from "@nestjs-modules/mailer";
 import { Injectable } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
-import * as fs from 'fs/promises';
-import * as ejs from 'ejs';
-import { join } from 'path';
+import { TemplatesService } from "../templates/templates.service";
 
 @Injectable()
 export class MailService {
-    constructor(private readonly mailerService: MailerService, private readonly configService: ConfigService) { }
+    constructor(
+        private readonly mailerService: MailerService,
+        private readonly configService: ConfigService,
+        private readonly templatesService: TemplatesService
+    ) { }
 
     async sendMail(to: string, subject: string, text: string) {
         await this.mailerService.sendMail({
@@ -16,10 +18,11 @@ export class MailService {
     }
 
     async sendTemplateMail(to: string, subject: string, template: string, context: object) {
-        const htmlTemplate = await fs.readFile(join(process.cwd(), 'assets', 'templates', `${template}.ejs`), 'utf8');
+        const html = await this.templatesService.getCompiledTemplate(template, context);
+
         return await this.mailerService.sendMail({
             from: this.configService.get('SMTP_FROM'),
-            to, subject, html: ejs.render(htmlTemplate, context)
+            to, subject, html
         })
     }
 }
