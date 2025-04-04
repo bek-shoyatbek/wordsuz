12fe998ab81d8720acec8d783a34392414957f3a
bek-shoyatbek <shoyatbekov03032003@gmail.com>
2025-04-04 18:26:25 +0500

    chore: updated env secrets in .env.example and .env.config.ts

--- src/shared/configs/env.config.ts
+++ src/shared/configs/env.config.ts
@@ -6,8 +6,28 @@ export const envConfig = cleanEnv(process.env, {
 
     DATABASE_URL: url(),
 
+    REDIS_HOST: str(),
+    REDIS_PORT: num(),
+    REDIS_PASSWORD: str(),
+
+    PEXELS_API_KEY: str(),
+
     WORDS_API_URL: url(),
     WORDS_API_KEY: str(),
     WORDS_API_HOST: str(),
+
     TRANSLATOR_API_KEY: str(),
+
+    SMTP_HOST: str(),
+    SMTP_PORT: num(),
+    SMTP_LOGIN: str(),
+    SMTP_FROM: str(),
+    SMTP_KEY: str(),
+
+    JWT_SECRET: str(),
+    JWT_EXPIRES_IN: str(),
+    JWT_ACCESS_TOKEN_SECRET: str(),
+    JWT_ACCESS_TOKEN_EXPIRES_IN: str(),
+    JWT_REFRESH_TOKEN_SECRET: str(),
+    JWT_REFRESH_TOKEN_EXPIRES_IN: str(),
 })
